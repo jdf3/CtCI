1e98113ea29432eb5e87f41af35b2ae0e7926668
Jeremy Fehr <Jeremy Fehr>
2017-04-18 23:55:27 -0700

    rest of 8.2

--- python/8-recursion-and-dynamic-programming/robotgrid.py
+++ python/8-recursion-and-dynamic-programming/robotgrid.py
@@ -36,3 +36,31 @@ grid = [[0, 0, 0, 0, 1],
         [1, 0, 0, 0, 0]]
 
 print(get_path(grid))
+
+def get_path_bottom_up(grid):
+  p = find_path_bottom_up(grid, (0, 0), (len(grid) - 1, len(grid[0]) - 1))
+  return p
+
+def find_path_bottom_up(grid, pos, target):
+  path = []
+  if grid[pos[0]][pos[1]] == 1: return path
+  path.append(pos)
+
+  visited = set()
+
+  while len(path) > 0 and path[-1] != target:
+    pos = path[-1]
+    if pos[0] < len(grid) - 1 and grid[pos[0]+1][pos[1]] != 1 and not (pos[0] + 1, pos[1]) in visited:
+      new_pos = (pos[0] + 1, pos[1])
+      path.append(new_pos)
+      visited.add(new_pos)
+    elif pos[1] < len(grid[0]) - 1 and grid[pos[0]][pos[1]+1] != 1 and not (pos[0], pos[1] + 1) in visited:
+      new_pos = (pos[0], pos[1] + 1)
+      path.append(new_pos)
+      visited.add(new_pos)
+    else:
+      path.pop()
+
+  return path
+
+print(get_path_bottom_up(grid))
