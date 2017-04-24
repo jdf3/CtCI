d867316b927d9af18eef27d350efe2949b0f0c95
Jeremy Fehr <jfehr7212@gmail.com>
2017-04-24 10:49:16 -0700

    8.13

---
+++ python/8-recursion-and-dynamic-programming/stackofboxes.py
@@ -0,0 +1,24 @@
+# You have a stack of n boxes, with widths w_i, heights h_i, and depths d_i.
+# The boxes cannot be rotated and can only be stack on top of one another if
+# each box in the stack is strictly larger than the box above it in width,
+# height, and depth. Implement a method to compute the height of the tallest
+# possible stack. The height of a stack is the sum of the heights of each box.
+
+def stackofboxes(stack):
+  memo = [-1]*len(stack)
+  def r(stack, stackpos, basewidth, baseheight, basedepth):
+    if memo[stackpos] != -1: return memo[stackpos]
+    if stackpos >= len(stack): return 0
+    poss = [0]
+    for i in range(stackpos, len(stack)):
+      w, h, d = stack[i]
+      if w < basewidth and h < baseheight and d < basedepth:
+        poss.append(h + r(stack, i + 1, w, h, d))
+
+    return max(poss)
+
+  copy = stack[:]
+  copy.sort(reverse=True)
+  return r(copy, 0, 1 << 32, 1 << 32, 1 << 32)
+
+print(stackofboxes([(1, 1, 1), (2, 2, 2), (2.5, 1, 1.75)]))
